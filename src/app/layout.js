9277af4c5342351db5ec4cef290280776f3f1573
Muhammad Sufyan <sufidemo@gmail.com>
2025-01-04 18:38:31 +0500

    auth ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito as NunitoFont } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const nunito = NunitoFont({
   subsets: ["latin"],
 });
 
@@ -20,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunito.className} antialiased`}
       >
         {children}
       </body>
